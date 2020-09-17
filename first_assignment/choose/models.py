65b02a82342d4b32ecad5c87743d0697a1d4e513
hmm6235 <hmm6235@gmail.com>
2020-09-18 03:02:58 +0900

    크크

--- first_assignment/choose/models.py
+++ first_assignment/choose/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    address = models.CharField(max_length = 100)
     name=models.CharField(max_length=20)
     title=models.CharField(max_length=200)
+    image = models.ImageField('img/')
     pub_date=models.DateTimeField('date published')
     body = models.TextField()
 
--- first_assignment/firstproject/urls.py
+++ first_assignment/firstproject/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 import choose.views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('atm/',choose.views.choose_atm, name="choose_atm"),
     path('choose/',choose.views.choose3, name="choose3"),
     path('home/',choose.views.home, name="home"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

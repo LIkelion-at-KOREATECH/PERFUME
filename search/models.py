563144c84e796aa5ecb70af170364dff3379833a
qkrghd147 <qkrghd147@likelion.org>
2020-09-19 05:00:55 +0900

    users의 모델과 search 의 모델을 통일시켰습니다,

--- search/models.py
+++ search/models.py
@@ -4,12 +4,14 @@ from django.db import models
 
 # class Post(models.Model):
 class Blog(models.Model):
-    image = models.ImageField(upload_to = 'image/')
+    image = models.ImageField(blank=True, upload_to='media/')
+    address = models.CharField(max_length = 100)
+    atm = models.CharField(max_length=100)
     name = models.CharField(max_length=20)
     title = models.CharField(max_length=200)
     body = models.TextField()
-    pub_date = models.DateTimeField('date published')
-    address = models.CharField(max_length = 100)
+    registered_dttm = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
